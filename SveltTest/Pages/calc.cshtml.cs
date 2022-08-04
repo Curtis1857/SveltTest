b002cd6790e88263664afd2cb4409f111acd760c
curti <curti@TAMMY>
2022-08-03 22:49:09 -0400

    Add project files.

---
+++ SveltTest/Pages/calc.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SveltTest.Pages
+{
+    public class calcModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SveltTest/wwwroot/js/calc.svelte
@@ -0,0 +1,33 @@
+﻿<svelte:options tag="calc-c" />
+<script>
+    let sum;
+    let f = 1;
+    let s = 3;
+
+    function getSum(ff, ss){
+         console.log(ff);
+         console.log(ss);
+         if(ff && ss){
+             var nu = Number(ff) + Number(ss);
+             console.log(nu);
+             return nu;
+         } else return 0;
+    }
+
+    $: sum = getSum(f,s);
+    
+</script>
+
+<main>
+    <input bind:value={f} />
+    <input bind:value={s}  />
+    <div>
+    {sum}
+    </div>
+</main>
+
+<style>
+    h1 {
+        font-size: 5em;
+    }
+</style>
